694194482e7f17604c4cf0ab6a423485e488c502
DudeBro249 <yoayush@gmail.com>
2021-04-14 19:39:29 +0530

    switched models to use pydantic

--- models/channel.py
+++ models/channel.py
@@ -1,45 +1,11 @@
-class ChannelOut:
+from pydantic import BaseModel
+
+class ChannelOut(BaseModel):
     channel_record_id: int
     channel_id: str
     guild_id: str
 
-    def __init__(self, channel_record_id: int, channel_id: str, guild_id: str) -> None:
-        self.channel_record_id = channel_record_id
-        self.channel_id = channel_id
-        self.guild_id = guild_id
 
-    @staticmethod
-    def from_dict(obj: dict) -> 'ChannelIn':
-        channel_record_id = int(obj.get("channel_record_id"))
-        channel_id = str(obj.get("channel_id"))
-        guild_id = str(obj.get("guild_id"))
-        return ChannelIn(channel_record_id, channel_id, guild_id)
-
-    def to_dict(self) -> dict:
-        result: dict = {}
-        result["channel_record_id"] = int(self.channel_record_id)
-        result["channel_id"] = str(self.channel_id)
-        result["guild_id"] = str(self.guild_id)
-        return result
-
-
-
-class ChannelIn:
+class ChannelIn(BaseModel):
     channel_id: str
     guild_id: str
-
-    def __init__(self, channel_id: str, guild_id: str) -> None:
-        self.channel_id = channel_id
-        self.guild_id = guild_id
-
-    @staticmethod
-    def from_dict(obj: dict) -> 'ChannelIn':
-        channel_id = str(obj.get("channel_id"))
-        guild_id = str(obj.get("guild_id"))
-        return ChannelIn(channel_id, guild_id)
-
-    def to_dict(self) -> dict:
-        result: dict = {}
-        result["channel_id"] = str(self.channel_id)
-        result["guild_id"] = str(self.guild_id)
-        return result
